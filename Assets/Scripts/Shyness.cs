bfe1b8d49d3c06f260d723a905476c37779627c0
pr00thmatic <vengadoravg@gmail.com>
2019-07-14 09:52:43 -0400

    Feature/shy structures (#11)
    * flipping normals
    
    * shy wall tiles
    
    * baked :O
    
    * shy floors and roofs
    
    * shy light bulbs
    
    * an editor arrow to indicate the direction of the target :3
    
    * oops... forgot to erase debugging variable

---
+++ Assets/Scripts/Shyness.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+using System.Collections;
+using System.Collections.Generic;
+
+[ExecuteInEditMode]
+public class Shyness : MonoBehaviour {
+    public Vector3 forward = new Vector3(0,0,1);
+
+    public Transform target;
+    public Renderer[] cachedRenderers;
+    public Renderer visibilityProbe;
+
+    public Vector3 GlobalForward { get => target.TransformDirection(forward); }
+
+    void Awake () {
+        if (cachedRenderers == null || cachedRenderers.Length == 0) {
+            cachedRenderers = target.GetComponentsInChildren<Renderer>();
+        }
+    }
+
+    void Update () {
+        if (!visibilityProbe.isVisible) return;
+        Vector3 view = Camera.main.transform.position - transform.position;
+        float difference = Vector3.SignedAngle(view, GlobalForward, target.up);
+        bool isShy = difference < -90 || difference > 90;
+
+        foreach (Renderer r in cachedRenderers) {
+            if (!r.gameObject.activeInHierarchy) continue;
+
+            if (!isShy || Util.FindInParent<NotShy>(r.transform) != null) {
+                r.shadowCastingMode = ShadowCastingMode.On;
+            } else if (isShy) {
+                r.shadowCastingMode = ShadowCastingMode.ShadowsOnly;
+            }
+        }
+    }
+}
